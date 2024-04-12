fc4351dad58b3cc6f59951c4c640001eaeb58c19
iamkevin42 <113134296+iamkevin42@users.noreply.github.com>
2024-04-12 13:27:56 -0700

    print

---
+++ assignment2.py
@@ -0,0 +1,13 @@
+"""##### Task 2
+(2 points) **Create** a new assignment file called "assignment2.py".  It should display the following:
+
+Sample Output Task #2
+This is my second program.
+It uses "two commands" to display the output.
+
+Note that you can't use the command print("It uses "two commands" to display the output.") because the Python3 interpreter sees the second double quotation is ending the first one.  In this case, we have two choices:
+  - Use a single quotation mark as the beginning and end of the output string.
+  - "Escape" the internal double quotation mark.  Using \" instead of " will tell the compiler that the double quote is for display, and is not functional
+"""
+
+print('Hello World,\"My name is Kevin\"')
---
+++ assignment5.py
@@ -0,0 +1,25 @@
+'''Task 5
+(2 points)
+Create a file called "assignment5.py" in your project.  We are going to have your program display the clasic poem, "Ozymandias" by Percy Bysshe Shelly.
+Your program should display a title on one line, the author's name on the line below it and then a couple spaces before you display the poem.
+https://www.poetryfoundation.org/poems/46565/ozymandias
+'''
+
+print("Ozymandias")
+print('By Percy Bysshe Shelley: \n\n')
+
+
+print("I met a traveller from an antique land,")
+print("Who said—“Two vast and trunkless legs of stone")
+print("Stand in the desert. . . . Near them, on the sand,")
+print("Half sunk a shattered visage lies, whose frown,")
+print("And wrinkled lip, and sneer of cold command,")
+print("Tell that its sculptor well those passions read")
+print("Which yet survive, stamped on these lifeless things,")
+print("The hand that mocked them, and the heart that fed;")
+print("And on the pedestal, these words appear:")
+print("My name is Ozymandias, King of Kings;")
+print("Look on my Works, ye Mighty, and despair!")
+print("Nothing beside remains. Round the decay")
+print("Of that colossal Wreck, boundless and bare")
+print("The lone and level sands stretch far away.")
